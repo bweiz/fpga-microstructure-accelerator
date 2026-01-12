759e4584db7c726a39a81881e7942dc69cdfbcb8
bweiz <ben.weizenegger@gmail.com>
2026-01-11 17:37:56 -0700

    Added avs read_data functionality, working on avs_write

--- hw/hdl/mstr-mmio/mstr-mmio.vhd
+++ hw/hdl/mstr-mmio/mstr-mmio.vhd
@@ -43,8 +43,8 @@ end entity mstr_mmio;
 
 architecture rtl of mstr_mmio is
 
-  ID_CONST                : std_logic_vector(31 downto 0) := x"4D535452";
-  VERSION_CONST           : std_logic_vector(31 downto 0) := x"00010000";
+  constant ID_CONST       : std_logic_vector(31 downto 0) := x"4D535452";
+  constant VERSION_CONST  : std_logic_vector(31 downto 0) := x"00010000";
   signal reg_run_enable   : std_logic;
   signal reg_pulse_reset  : std_logic;
   -- Will change following to on reset behaviour, not := xxx
@@ -54,6 +54,39 @@ architecture rtl of mstr_mmio is
 
 begin
 
+  avalon_register_read : process(clk)
+  begin
+    if rising_edge(clk) then
+      if avs_read = '1' then
+        case address is
+          when "00000"      => avs_readdata <= ID_CONST;
+          when "00001"      => avs_readdata <= VERSION_CONST;
+          when "00010"      => avs_readdata <= (31 downto 1 => '0') & reg_run_enable;
+          when "00011"      => avs_readdata <= (31 downto 1 => '0') & reg_run_enable; --subject to change
+          when "00100"      => avs_readdata <= reg_bucket_ns;
+          when "00101"      => avs_readdata <= reg_vwap_t_ns;
+          when "00110"      => avs_readdata <= reg_mp_frac_bits;
+          when others       => avs_readdata <= (others => '0');
+        end case;
+      end if;
+    end if;
+  end process;
 
-
+  avalon_register_write : process(clk, rst)
+  begin
+    if rst = '1' then
+      reg_bucket_ns     := x"000F4240";
+      reg_vwap_t_ns     := (others => '0');
+      reg_mp_frac_bits  := x"08";
+    elsif rising_edge(clk) then
+      if avs_write = '1' then
+        case address is
+          when "00010"      => avs_writedata <= reg_run_enable;
+          when "00100"      => avs_writedata <= reg_bucket_ns;
+          when "00101"      => avs_writedata <= reg_vwap_t_ns;
+          when "00110"      => avs_writedata <= reg_mp_frac_bits;
+          when others       => avs_writedata <= (others => '0');
+        end case;
+      end if;
+    end if;
 end architecture rtl;
